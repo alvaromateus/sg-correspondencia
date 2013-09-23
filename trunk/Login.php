af33bd19f80a6d3d59f30f39b6f296a8635ebf7b
willmersdorffernando@gmail.com <willmersdorffernando@gmail.com@bce10ebf-7725-bd8b-df62-6cb74e7bcb13>
2013-09-23 17:29:18 +0000

    Criação da tela de Login, confirmação de acesso e Formatação dos elementos na tela.

--- trunk/Login.php
+++ trunk/Login.php
@@ -2,22 +2,22 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link rel="stylesheet" type="text/css" href="../Css/Formatacao.css">
+        <link rel="stylesheet" type="text/css" href="Formatacao.css">
         <title>.: SG Correspondência :.</title>
     </head>
     <body>
         <div class="topo">
-            .: Bem vindo ao sistema Gen-U :.
+            .: Sistema de Gerenciamento de Correspondência - SGC :.
         </div>
         <form method="post" action="">
-        <input type="hidden" name="Consultar" value="">
-        <div class="logar">
-            <label class="lb">Usuário:</label>
-            <input type="text" name="txtusuario" class="txt" title="Digite o nome de usuário."/>
-            <label class="lb">Senha:</label>
-            <input type="password" name="txtsenha" class="txt" title="Digite a senha."/>
-            <input type="submit" value="Entrar" name="btentrar" onClick="Consultar.value='GERAR';" class="bt">
-        </div>
+            <input type="hidden" name="Consultar" value="">
+            <div class="logar">
+                <label class="lb">Usuário:</label>
+                <input type="text" name="txtusuario" class="txt" title="Digite o nome de usuário."/>
+                <label class="lb">Senha:</label>
+                <input type="password" name="txtsenha" class="txt" title="Digite a senha de acesso."/>
+                <input type="submit" value="Entrar" name="btentrar" onClick="Consultar.value='GERAR';" class="bt">
+            </div>
         </form>
     </body>
 </html>
