200381554dd610f1887be4c8e7d996ad5f5b5908
willmersdorffernando <willmersdorffernando@gmail.com>
2013-10-14 17:45:55 +0000

    Conectando ao banco usando a função md5 para a senha.

--- ConsultarUsuario.php
+++ ConsultarUsuario.php
@@ -12,8 +12,8 @@
             if($_POST['Consultar'] == "GERAR")
             {
                 $usuario = $_POST['txtusuario'];
-                $acesso = $_POST['txtsenha'];
-                $stmt = oci_parse($conexao, "SELECT u.nm_usuario, u.nm_senha, f.nm_funcionario FROM Usuario u, Funcionario f WHERE nm_senha = $acesso AND u.cd_registro = f.cd_registro");
+                $acesso = md5($_POST['txtsenha']);
+                $stmt = oci_parse($conexao, "SELECT u.nm_usuario, u.nm_senha, f.nm_funcionario FROM Usuario u, Funcionario f WHERE u.nm_usuario = '$usuario' AND u.cd_registro = f.cd_registro");
                 oci_execute($stmt, OCI_DEFAULT);
                 while($row = oci_fetch_array($stmt))
                 {
