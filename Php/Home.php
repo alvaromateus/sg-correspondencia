669c719157aace409e8430954c4c536373d66727
willmersdorffernando <willmersdorffernando@gmail.com>
2013-10-17 21:23:07 +0000

    Criação da home e associação das páginas menu e usuário a ela.

---
+++ Php/Home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel="stylesheet" type="text/css" href="../Formatação/FormatacaoHome.css">
+        <title>.: SG - Correspondência :.</title>
+    </head>
+    <body>
+        <div id="Principal">
+            <div id="Login">
+                <?php
+                    require 'ConsultaUsuario.php';
+                ?>
+            </div>
+            <div id="Menu">
+                <?php
+                    require 'Menu.php';
+                ?>
+            </div>
+            <div id="Conteudo">
+                <h1>Pagina Centrada com CSS sem scroll até 800x600</h1>
+                <h1>Pagina Centrada com CSS sem scroll até 800x600</h1>
+                <h1>Pagina Centrada com CSS sem scroll até 800x600</h1>
+                <h1>Pagina Centrada com CSS sem scroll até 800x600</h1>
+                <h1>Pagina Centrada com CSS sem scroll até 800x600</h1>
+                <h1>Pagina Centrada com CSS sem scroll até 800x600</h1>
+            </div>
+        </div>
+    </body>
+</html>
