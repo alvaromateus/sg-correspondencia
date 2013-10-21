94c1607f6cae6bc635f9e045a88314b4d3b90a91
willmersdorffernando@gmail.com <willmersdorffernando@gmail.com@bce10ebf-7725-bd8b-df62-6cb74e7bcb13>
2013-10-21 21:08:06 +0000

    Criação dos insertes.

--- trunk/Php/Insercao.php
+++ trunk/Php/Insercao.php
@@ -4,15 +4,18 @@
     {
         if($_POST['Inserir'] == "MALOTE")
         {
-            $sql = oci_parse($conexao, 'INSERT INTO Malote (cd_malote, nm_origem, nm_destino) VALUES (:malote, :origem, :destino)');
+            $sql = oci_parse($conexao, 'INSERT INTO Malote (cd_malote, nm_origem, nm_destino, qt_correspondencia) VALUES (:malote, :origem, :destino, :quantidade)');
             $malote = $_POST['txtnumero'];
             $origem = $_POST['txtorigem'];
             $destino = $_POST['txtdestino'];
+            $quantidade = $_POST['txtquantidade'];
             oci_bind_by_name($sql, ':malote', $malote);
             oci_bind_by_name($sql, ':origem', $origem);
             oci_bind_by_name($sql, ':destino', $destino);
+            oci_bind_by_name($sql, ':quantidade', $quantidade);
             oci_execute($sql);
             oci_free_statement($sql);
+            header('Location: /SGC/trunk/Php/CadMalote.php?Malote=1');
         }
     }
 ?>
