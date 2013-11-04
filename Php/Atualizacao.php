069392dc3d3ebf9e98814728e75af3c6f297d21e
willmersdorffernando <willmersdorffernando@gmail.com>
2013-11-04 11:51:54 +0000

    Concluída a página de atualização e exclusão de usuário.

--- Php/Atualizacao.php
+++ Php/Atualizacao.php
@@ -4,18 +4,18 @@
     {
         if($_POST['Atualizar'] == "USUARIO")
         {
-            $sql = oci_parse($conexao, 'INSERT INTO Malote (cd_malote, nm_origem, nm_destino, qt_correspondencia) VALUES (:malote, :origem, :destino, :quantidade)');
-            $malote = $_POST['txtnumero'];
-            $origem = $_POST['txtorigem'];
-            $destino = $_POST['txtdestino'];
-            $quantidade = $_POST['txtquantidade'];
-            oci_bind_by_name($sql, ':malote', $malote);
-            oci_bind_by_name($sql, ':origem', $origem);
-            oci_bind_by_name($sql, ':destino', $destino);
-            oci_bind_by_name($sql, ':quantidade', $quantidade);
+            $cregistro = $_POST['txtregistro'];
+            $cusuario = $_POST['txtusuario'];
+            $csenha = $_POST['txtsenha'];
+            $cacesso = $_POST['txtacesso'];
+            $sql = oci_parse($conexao, 'UPDATE Usuario SET nm_usuario = :usuario, nm_senha = :senha, nm_acesso = :acesso WHERE cd_registro = :registro');
+            oci_bind_by_name($sql, ':registro', $cregistro);
+            oci_bind_by_name($sql, ':usuario', $cusuario);
+            oci_bind_by_name($sql, ':senha', $csenha);
+            oci_bind_by_name($sql, ':acesso', $cacesso);
             oci_execute($sql);
             oci_free_statement($sql);
-            header('Location: /SGC/trunk/Php/CadMalote.php?Malote=1');
+            header('Location: ConUsuario.php');
         }
     }
 ?>
